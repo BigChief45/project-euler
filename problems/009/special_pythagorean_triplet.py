a29c72377e585c0c837720b3ebf425cde75be39a
Jaime Alvarez <jaime_andres@unitec.edu>
2016-03-23 09:46:33 +0000

    Change problems directory names

---
+++ problems/009/special_pythagorean_triplet.py
@@ -0,0 +1,61 @@
+### Jaime Alvarez - March 2016
+
+"""
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+# Method 1: Two-Parameter Generator Set
+# When m and n are any two positive integers (m < n):
+# a = n^2 - m^2
+# b = 2nm
+# c = n^2 + m^2
+m = 1
+n = 2
+a,b,c = 0,0,0
+
+product = 0
+
+# We are going to create triples until a set of triples a + b + c = 1000 is satisfied.
+while (a + b + c) <= 1000:
+    # Generate triple
+    a = (n**2) - (m**2)
+    b = 2*n*m
+    c = (n**2) + (m**2)
+
+    print "(%s, %s, %s)" % (a,b,c)
+
+    m += 1
+    n += 1
+
+# Check if our triple sums to 1000
+if (a + b + c) == 1000:
+    # Once the triple has been found, let's get the product
+    print "Triple Found: (%s, %s, %s" % (a,b,c)
+    product = a * b * c
+else:
+    print "Triple not found, using Method #2..."
+    
+    # Triple was not found, we must use a different method
+    a = 3
+    b = 4
+    c = 5
+    n = 2
+    while (a + b + c) <= 1000:
+        a *= n
+        b *= n
+        c *= n
+        print "(%s, %s, %s)" % (a,b,c)    
+        #n += 1
+
+# Check if the triple sums to 1000
+if (a + b + c) == 1000:
+    print "Triple Found: (%s, %s, %s" % (a,b,c)
+    product = a * b * c
+    
+    
+print "abc = ", product
