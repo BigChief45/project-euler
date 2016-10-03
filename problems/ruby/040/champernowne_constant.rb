3a81e90ca5eaa3ad545add7748ee7853ec9ca63e
Jaime Alvarez <jaime_andres@unitec.edu>
2016-10-03 02:46:01 +0000

    Problem #40 Solution (Ruby)

---
+++ problems/ruby/040/champernowne_constant.rb
@@ -0,0 +1,18 @@
+## Jaime Alvarez -- October 2016
+
+=begin
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.123456789101112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+=end
+
+seq = (1..1000000).to_a.join
+answer = seq[1-1].to_i * seq[10-1].to_i * seq[100-1].to_i * seq[1000-1].to_i * seq[10000-1].to_i * seq[100000-1].to_i * seq[1000000-1].to_i
+
+puts "Answer: #{answer}"
